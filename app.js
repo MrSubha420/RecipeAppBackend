044ba012a0118f969a969af244d9a03931402075
Subhadip Manna <codewithsubh2.0@gmail.com>
2024-10-27 13:06:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import dbConnection from "./Database/dbConnection.js";
 import recipeRoutes from "./Routes/RecipeRoutes.js";
 import dotenv from "dotenv";
+import fileUpload from "express-fileupload";
+import cookieParser from "cookie-parser";
 
 // Load environment variables
 dotenv.config({path: "./config/config.env"})
@@ -13,10 +15,18 @@ const app = express();
 dbConnection();
 
 // Middleware
+app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+   origin: [process.env.DASHBOARD_URL],
+   methods: ["GET","POST","DELETE","PUT"],
+   credentials : true,
+}));
 app.use(express.urlencoded({ extended: true })); // Enables URL-encoded data parsing
-
+// app.use(fileUpload({
+//    useTempFiles: true,
+//    tempFileDir: "/tmp/",
+// }));
 // Routes
 app.use("/api/v1/recipe", recipeRoutes);
 
