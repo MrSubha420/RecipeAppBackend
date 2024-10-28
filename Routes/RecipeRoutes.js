28ef0e8d6f36cb0732cc9b618c71f29a7b90e4e2
Subhadip Manna <codewithsubh2.0@gmail.com>
2024-10-28 15:11:42 +0530

    Update RecipeRoutes.js

--- Routes/RecipeRoutes.js
+++ Routes/RecipeRoutes.js
@@ -5,8 +5,8 @@ import { upload } from '../config/cloudinary.js';
 const router = express.Router();
 
 router.post('/create', upload.single('image'), createRecipe);
-router.get('/all', getAllRecipes); // Adjusted from 'gateall' to 'all' for clarity
-router.get('/:id', getRecipeById);
+router.get('/gateall', getAllRecipes); // Adjusted from 'gateall' to 'all' for clarity
+router.get('/singleRecipe/:id', getRecipeById);
 router.put('/update/:id', upload.single('image'), updateRecipe);
 router.delete('/delete/:id', deleteRecipe);
 
